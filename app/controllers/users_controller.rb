b09a4ba4d5fe458a390f318e150d1d3ecabff3b3
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-08-30 12:14:22 -0400

    Add User controller actions

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user, notice: "Successfully signed up!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :age, :favorite_genre, :user_name, :password,
+                                 :password_confirmation)
+  end
+end
