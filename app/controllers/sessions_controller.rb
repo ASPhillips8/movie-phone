bcbe4e95490ecc8224cd56500f13f1d5f7fd8a9e
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-08-30 13:56:44 -0400

    Add sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(user_name: params[:user_name])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user_path
+    else
+      flash[:alert] = "Invalid name or passwrod"
+      render :new
+    end
+  end
+
+  def destroy
+  end
+end
