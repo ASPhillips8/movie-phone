d289897bc3e38f864150dcac9d3bd63629ca945d
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-08-30 16:16:10 -0400

    Add session creation on user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :require_login, except: %i[new create]
+
   def new
     @user = User.new
   end
@@ -10,14 +12,24 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to @user, notice: "Successfully signed up!"
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+private
+
   def user_params
     params.require(:user).permit(:first_name, :last_name, :age, :favorite_genre, :user_name, :password,
                                  :password_confirmation)
+  end
+
+  def require_login
+    return if current_user
+
+    flash[:alert] = "You must be logged in to access this section"
+    redirect_to root_path
   end
 end
