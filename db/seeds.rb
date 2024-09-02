31e2a4a3a252ffb9283d030e8de6f2334157d28e
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-09-01 20:40:28 -0400

    Update seed date to add customers and reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,8 @@
 require "faker"
 
 User.destroy_all
+Movie.destroy_all
+Review.destroy_all
 
 movie_genres = %w[
   Action Comedy Drama Horror Romance Sci-Fi Thriller Fantasy
@@ -31,3 +33,33 @@ movie_genres = %w[
 end
 
 puts "5 fake users created"
+
+# Create 10 fake movies
+10.times do
+  Movie.create!(
+    title: Faker::Movie.title,
+    description: Faker::Lorem.paragraph(sentence_count: 5),
+    run_time: Faker::Number.between(from: 60, to: 180), # Adjusted runtime to be between 60 and 180 minutes
+    rating: %w[G PG PG-13 R].sample,
+    release_date: Faker::Date.between(from: "1980-01-01", to: Date.today)
+  )
+end
+
+puts "10 fake movies created"
+
+# Create reviews for each movie ensuring one review per user per movie
+Movie.all.each do |movie|
+  # Select a random number of users to leave a review for this movie
+  users = User.all.sample(rand(1..5))
+
+  users.each do |user|
+    Review.create!(
+      score: Faker::Number.between(from: 0, to: 10), # Adjusted score to be between 0 and 10
+      comment: Faker::Lorem.sentence(word_count: 10),
+      user: user,
+      movie: movie
+    )
+  end
+end
+
+puts "Reviews created for each movie"
