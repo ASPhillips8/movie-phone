83ec8462a2f9330c06511979acd577cc5ba05a81
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-09-02 13:42:44 -0400

    Add validation for one review from single user

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,5 @@ class Review < ApplicationRecord
 
   validates :score, presence: true,
                     numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
+  validates :user_id, uniqueness: { scope: :movie_id, message: "can only leave one review per movie" }
 end
