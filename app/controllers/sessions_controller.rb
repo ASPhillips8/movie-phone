6bd56d93a83196f0fbebba9d5aedaba6e5871e21
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-09-04 11:53:33 -0400

    rubocop corrections

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(user_name: params[:user_name])
 
-    if user && user.authenticate(params[:password])
+    if user&.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to user_path(user)
     else
