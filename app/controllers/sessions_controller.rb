e221af473e633f83c52a7b61023282dc437e51ac
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-09-04 09:55:30 -0400

    Signin Flash alert and controller change

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,13 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(user_name: params[:user_name])
+
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to user_path(user)
     else
-      @login_errors = ["Invalid name or password"]
-      Rails.logger.debug "Login Errors: #{@login_errors.inspect}"
-      render :new
+      flash[:alert] = "Invalid user name or password"
+      redirect_to signin_path
     end
   end
 
