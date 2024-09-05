39d9510b294fdaf1dca69a8fcbf027e1153371d7
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-09-05 10:13:25 -0400

    Add test for age_based_movies

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { build(:user) } # Use FactoryBot to build a user instance
+  let(:user) { build(:user) }
 
   context "validations" do
     it "is valid with all attributes present" do
@@ -17,7 +17,7 @@ RSpec.describe User, type: :model do
     end
 
     it "is invalid with a duplicate user name" do
-      create(:user, user_name: "Lumpy3") # Create a user with a duplicate user_name
+      create(:user, user_name: "Lumpy3")
       expect(user).not_to be_valid
       expect(user.errors[:user_name]).to include("has already been taken")
     end
@@ -46,6 +46,45 @@ RSpec.describe User, type: :model do
     describe "#full_name" do
       it "returns the user's full name" do
         expect(user.full_name).to eq("Anthony Lumpenstein")
+      end
+    end
+
+    describe "age_based_movies" do
+
+      it "returns movies that are G and PG of ages 0-12" do
+        user = build(:user, age: 10)
+        g_movie = create(:movie, rating: "G")
+        pg_movie = create(:movie, rating: "PG")
+        pg13_movie = create(:movie, rating: "PG-13")
+        r_movie = create(:movie, rating: "R")
+
+        movie_age = user.age_based_movies
+        expect(movie_age).to include(g_movie, pg_movie)
+        expect(movie_age).not_to include(pg13_movie, r_movie)
+      end
+
+      it "returns movies that are G, PG, PG-13 for ages 13-16" do
+        user = build(:user, age: 13)
+        g_movie = create(:movie, rating: "G")
+        pg_movie = create(:movie, rating: "PG")
+        pg13_movie = create(:movie, rating: "PG-13")
+        r_movie = create(:movie, rating: "R")
+
+        movie_age = user.age_based_movies
+        expect(movie_age).to include(g_movie, pg_movie, pg13_movie)
+        expect(movie_age).not_to include(r_movie)
+
+      end
+
+      it "returns movies that are all rating for ages 17 and up" do
+        user = build(:user, age: 18)
+        g_movie = create(:movie, rating: "G")
+        pg_movie = create(:movie, rating: "PG")
+        pg13_movie = create(:movie, rating: "PG-13")
+        r_movie = create(:movie, rating: "R")
+
+        movie_age = user.age_based_movies
+        expect(movie_age).to include(g_movie, pg13_movie, pg_movie, r_movie)
       end
     end
   end
